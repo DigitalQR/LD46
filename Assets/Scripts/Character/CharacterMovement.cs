b2d7a1f0b7904fc1a11014e55f7b88f072305698
Sam Potter <sampot97@gmail.com>
2020-04-18 11:57:46 +0100

    [Initial Commit] Basic Movement

---
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MovementSettings
+{
+	[Header("Movement")]
+	public float MovementAcceleration = 200.0f;
+	public float MovementMaxSpeed = 10.0f;
+	public float MovementDrag = 10.0f;
+
+	[Header("Forces")]
+	public float ForceMaxSpeed = 1000.0f;
+	public float ForceDrag = 0.5f;
+	public Vector3 Gravity = new Vector3(0.0f, -9.8f, 0.0f);
+}
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMovement : MonoBehaviour
+{
+	[SerializeField]
+	private MovementSettings m_GroundSettings;
+
+	[SerializeField]
+	private MovementSettings m_AirSettings;
+	
+	private CharacterController m_Controller;
+	private Vector3 m_MovementInput;
+	private Vector3 m_ImpulseInput;
+
+	private Vector3 m_MovementVelocity;
+	private Vector3 m_ForceVelocity;
+	private bool m_WasGrounded;
+
+
+	private void Awake()
+	{
+		m_Controller = GetComponent<CharacterController>();
+	}
+	
+	// Can be both airborne and grounded for 1 frame
+	public bool IsAirborne
+	{
+		get => !m_Controller.isGrounded || !m_WasGrounded;
+	}
+
+	public bool IsGrounded
+	{
+		get => m_Controller.isGrounded;
+	}
+
+	private void Update()
+	{
+		// Apply movement
+		{
+			MovementSettings settings = IsAirborne ? m_AirSettings : m_GroundSettings;
+			Debug.LogFormat("IsAirborne {0}", IsAirborne);
+
+			// Update movement
+			Vector3 frameInput = Vector3.ClampMagnitude(m_MovementInput * settings.MovementAcceleration, settings.MovementAcceleration) * Time.deltaTime;
+			m_MovementVelocity = Vector3.ClampMagnitude(m_MovementVelocity + frameInput, settings.MovementMaxSpeed);
+
+			// Update forces
+			Vector3 forceFrameInput = settings.Gravity * Time.deltaTime + m_ImpulseInput;
+			m_ForceVelocity = Vector3.ClampMagnitude(m_ForceVelocity + forceFrameInput, settings.ForceMaxSpeed);
+
+			m_MovementVelocity = ApplyMovementInternal(m_MovementVelocity);
+			m_ForceVelocity = ApplyMovementInternal(m_ForceVelocity);
+
+			// Apply drag
+			m_MovementVelocity *= Mathf.Clamp01(1.0f - settings.MovementDrag * Time.deltaTime);
+			m_ForceVelocity *= Mathf.Clamp01(1.0f - settings.ForceDrag * Time.deltaTime);
+
+			// Reset
+			m_WasGrounded = m_Controller.isGrounded;
+			m_MovementInput = Vector3.zero;
+			m_ImpulseInput = Vector3.zero;
+		}
+	}
+
+	private Vector3 ApplyMovementInternal(Vector3 velocity)
+	{
+		Vector3 startPos = m_Controller.transform.position;
+		if (m_Controller.Move(velocity * Time.deltaTime) != CollisionFlags.None)
+		{
+			Vector3 endPos = m_Controller.transform.position;
+			return endPos - startPos;
+		}
+
+		return velocity;
+	}
+
+	public void AddMovement2D(Vector2 dir, float force = 1.0f)
+	{
+		Vector2 input = dir.normalized * force;
+		m_MovementInput += new Vector3(input.x, 0, input.y);
+	}
+
+	public void AddMovement3D(Vector3 dir, float force = 1.0f)
+	{
+		m_MovementInput += dir.normalized * force;
+	}
+
+	public void AddImpulse3D(Vector3 dir, float force = 1.0f)
+	{
+		m_ImpulseInput += dir.normalized * force;
+	}
+
+	public bool ApplyJump(float force)
+	{
+		// We have a frame where we can maintain airborne movement to mimic bunny hopping
+		if (IsGrounded || m_WasGrounded)
+		{
+			m_ForceVelocity = Vector3.zero;
+			AddImpulse3D(Vector3.up, force);
+			return true;
+		}
+
+		return false;
+	}
+}
---
+++ Assets/Scripts/Character/PlayerInput.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+	[SerializeField]
+	private Transform m_TargetDirection;
+
+	[SerializeField]
+	private CharacterMovement m_TargetMovement;
+
+	[SerializeField]
+	private float m_JumpForce = 10.0f;
+
+	private void Update()
+    {
+		Vector3 targetInput = Vector3.zero;
+
+		Vector3 forward = new Vector3(m_TargetDirection.forward.x, 0.0f, m_TargetDirection.forward.z).normalized;
+		Vector3 right = new Vector3(m_TargetDirection.right.x, 0.0f, m_TargetDirection.right.z).normalized;
+		
+		if (Input.GetKey(KeyCode.W))
+		{
+			targetInput += forward;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			targetInput -= forward;
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			targetInput -= right;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			targetInput += right;
+		}
+
+		if (Input.GetKey(KeyCode.Space))
+		{
+			m_TargetMovement.ApplyJump(m_JumpForce);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Tab))
+		{
+			if (Cursor.lockState == CursorLockMode.Locked)
+			{
+				Cursor.lockState = CursorLockMode.None;
+				Cursor.visible = true;
+			}
+			else
+			{
+				Cursor.lockState = CursorLockMode.Locked;
+				Cursor.visible = false;
+			}
+		}
+
+		if (Cursor.lockState == CursorLockMode.Locked)
+		{
+			bool invertY = false;
+			Vector2 mouseMovement = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y") * (invertY ? 1 : -1));
+
+			float mouseSensitivity = 1.0f;
+			Vector3 targetRotation = m_TargetDirection.eulerAngles;
+			targetRotation.x += mouseMovement.y * mouseSensitivity; // pitch
+			targetRotation.y += mouseMovement.x * mouseSensitivity; // yaw
+			
+			// Calculate the lerp amount, such that we get 99% of the way to our target in the specified time
+			//var positionLerpPct = 1f - Mathf.Exp((Mathf.Log(1f - 0.99f) / positionLerpTime) * Time.deltaTime);
+			float rotationLerpTime = 0.01f;
+			float rotationLerpPct = 1.0f - Mathf.Exp((Mathf.Log(1f - 0.99f) / rotationLerpTime) * Time.deltaTime);
+			
+			m_TargetDirection.eulerAngles = Vector3.Lerp(m_TargetDirection.eulerAngles, targetRotation, rotationLerpPct);
+		}
+
+		m_TargetMovement.AddMovement3D(targetInput);
+	}
+}
